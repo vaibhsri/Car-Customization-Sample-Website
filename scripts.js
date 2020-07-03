e062dd822af8d2aa2a6314ce4638e98da0244832
Vaibhav <vsv1998@gmail.com>
2020-07-03 21:25:10 +0530

    update 3

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ function load(){
 
     var name = localStorage.getItem('name');
 
-    if(name=='' || name == null){
+    if(name=='' || name == null || name == "null"){
         signin.innerHTML="sign in";
     }else{
         signin.innerHTML="Welcome, " + name ;
